a89561a11371d906ec5b1b47798186a15bf94aff
Nathan Borg <nathanborg@Nathans-MacBook-Air.local>
2026-08-08 08:33:23 +1000

    Redesign scaffold revision register

--- essdesign.client/dist/assets/thumbs-up-Db9bDBMS.js
+++ essdesign.client/dist/assets/thumbs-up-Db9bDBMS.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CgtGfEH2.js";/**
+import{c as a}from"./index-Bg-VJQP9.js";/**
  * @license lucide-react v1.14.0 - ISC
  *
  * This source code is licensed under the ISC license.
