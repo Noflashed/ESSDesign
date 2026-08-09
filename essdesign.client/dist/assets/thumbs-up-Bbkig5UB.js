0493771358bad56d00688eea690422a2d0873729
Nathan Borg <nathanborg@Nathans-MacBook-Air.local>
2026-08-10 09:39:47 +1000

    Keep register table bottom borders

--- essdesign.client/dist/assets/thumbs-up-Bbkig5UB.js
+++ essdesign.client/dist/assets/thumbs-up-Bbkig5UB.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CHN3VIgf.js";/**
+import{c as a}from"./index-Q-f8XnAL.js";/**
  * @license lucide-react v1.14.0 - ISC
  *
  * This source code is licensed under the ISC license.
